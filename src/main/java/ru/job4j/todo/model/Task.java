b15e8c09f9ee20d6653ddb74edd06d41c63c9efa
dimitriy <rhymister19@gmail.com>
2025-11-20 20:54:52 +0300

    create entity model

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,25 @@
+package ru.job4j.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String description;
+
+    private LocalDateTime created = LocalDateTime.now();
+
+    private boolean done;
+}
