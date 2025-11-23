f9820b7714b9c4c35c5631c6ec90aca90cce9f3e
dimitriy <rhymister19@gmail.com>
2025-11-24 00:01:34 +0300

    feat: implement user model and data store
    - Create User model class
    - Implement UserStore for database operations
    - Add user service layer (UserService)

---
+++ src/main/java/ru/job4j/todo/model/User.java
@@ -0,0 +1,31 @@
+package ru.job4j.todo.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime created = LocalDateTime.now();
+}
---
+++ src/main/java/ru/job4j/todo/service/UserService.java
@@ -0,0 +1,26 @@
+package ru.job4j.todo.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.job4j.todo.model.User;
+import ru.job4j.todo.store.UserStore;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserStore userStore;
+
+    public Optional<User> save(User user) {
+        return Optional.ofNullable(userStore.createUser(user));
+    }
+
+    public Optional<User> findByLoginAndPassword(String login, String password) {
+        return userStore.findByLoginAndPassword(login, password);
+    }
+
+    public boolean isLoginExists(String login) {
+        return userStore.findByLogin(login).isPresent();
+    }
+}
