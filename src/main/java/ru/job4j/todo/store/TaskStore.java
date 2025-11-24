a5139f896f7c978b0ad352bb882ef21e97e60ae8
dimitriy <rhymister19@gmail.com>
2025-11-24 18:34:45 +0300

    6. Лямбды и шаблон Command [#49295]
    - Replace SessionFactory with CrudRepository in TaskStore and UserStore
    - Remove duplicate transaction management code
    - Utilize query(), optional() and run() methods from CrudRepository
    - Simplify data access layer architecture

--- src/main/java/ru/job4j/todo/store/TaskStore.java
+++ src/main/java/ru/job4j/todo/store/TaskStore.java
@@ -1,90 +1,67 @@
 package ru.job4j.todo.store;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 
 @Repository
 @AllArgsConstructor
 public class TaskStore {
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     public List<Task> findAll() {
-        return tx(session -> session.createQuery("from Task order by created desc", Task.class).list());
+        return crudRepository.query("from Task order by created desc", Task.class);
     }
 
     public List<Task> findCompleted() {
-        return tx(session -> session.createQuery("from Task where done = true order by created desc", Task.class).list());
+        return crudRepository.query("from Task where done = true order by created desc", Task.class);
     }
 
     public List<Task> findNew() {
-        return tx(session -> session.createQuery("from Task where done = false order by created desc", Task.class).list());
+        return crudRepository.query("from Task where done = false order by created desc", Task.class);
     }
 
     public Optional<Task> findById(int id) {
-        return tx(session -> {
-            Task task = session.get(Task.class, id);
-            return Optional.ofNullable(task);
-        });
+        return crudRepository.optional(
+                "from Task where id = :fId", Task.class,
+                Map.of("fId", id)
+        );
     }
 
     public Task createTask(Task task) {
-        return tx(session -> {
-            session.save(task);
-            return task;
-        });
+        crudRepository.run(session -> session.save(task));
+        return task;
     }
 
     public boolean deleteById(int id) {
-        return tx(session -> {
-            int deletedCount = session.createQuery("DELETE FROM Task WHERE id = :id")
-                    .setParameter("id", id)
-                    .executeUpdate();
-            return deletedCount > 0;
-        });
+        crudRepository.run(
+                "DELETE FROM Task WHERE id = :id",
+                Map.of("id", id)
+        );
+        return true;
     }
 
     public boolean completeTask(int id) {
-        return tx(session -> {
-            int updatedCount = session.createQuery("UPDATE Task SET done = true WHERE id = :id")
-                    .setParameter("id", id)
-                    .executeUpdate();
-            return updatedCount > 0;
-        });
+        crudRepository.run(
+                "UPDATE Task SET done = true WHERE id = :id",
+                Map.of("id", id)
+        );
+        return true;
     }
 
     public boolean updateTask(Task task) {
-        return tx(session -> {
-            int updatedCount = session.createQuery(
-                            "UPDATE Task SET description = :description, done = :done WHERE id = :id"
-                    )
-                    .setParameter("description", task.getDescription())
-                    .setParameter("done", task.isDone())
-                    .setParameter("id", task.getId())
-                    .executeUpdate();
-            return updatedCount > 0;
-        });
-    }
-
-    private <T> T tx(final Function<Session, T> command) {
-        final Session session = sf.openSession();
-        final Transaction tx = session.beginTransaction();
-        try {
-            T rsl = command.apply(session);
-            tx.commit();
-            return rsl;
-        } catch (final Exception e) {
-            session.getTransaction().rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
+        crudRepository.run(
+                "UPDATE Task SET description = :description, done = :done WHERE id = :id",
+                Map.of(
+                        "description", task.getDescription(),
+                        "done", task.isDone(),
+                        "id", task.getId()
+                )
+        );
+        return true;
     }
 }
--- src/main/java/ru/job4j/todo/store/UserStore.java
+++ src/main/java/ru/job4j/todo/store/UserStore.java
@@ -1,93 +1,69 @@
 package ru.job4j.todo.store;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.User;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 
 @Repository
 @AllArgsConstructor
 public class UserStore {
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     public List<User> findAll() {
-        return tx(session -> session.createQuery("from User order by created desc", User.class).list());
+        return crudRepository.query("from User order by created desc", User.class);
     }
 
     public Optional<User> findById(int id) {
-        return tx(session -> {
-            User user = session.get(User.class, id);
-            return Optional.ofNullable(user);
-        });
+        return crudRepository.optional(
+                "from User where id = :fId", User.class,
+                Map.of("fId", id)
+        );
     }
 
     public Optional<User> findByLogin(String login) {
-        return tx(session -> {
-            User user = session.createQuery("from User where login = :login", User.class)
-                    .setParameter("login", login)
-                    .uniqueResult();
-            return Optional.ofNullable(user);
-        });
+        return crudRepository.optional(
+                "from User where login = :login", User.class,
+                Map.of("login", login)
+        );
     }
 
     public Optional<User> findByLoginAndPassword(String login, String password) {
-        return tx(session -> {
-            User user = session.createQuery("from User where login = :login and password = :password", User.class)
-                    .setParameter("login", login)
-                    .setParameter("password", password)
-                    .uniqueResult();
-            return Optional.ofNullable(user);
-        });
+        return crudRepository.optional(
+                "from User where login = :login and password = :password", User.class,
+                Map.of(
+                        "login", login,
+                        "password", password
+                )
+        );
     }
 
     public User createUser(User user) {
-        return tx(session -> {
-            session.save(user);
-            return user;
-        });
+        crudRepository.run(session -> session.save(user));
+        return user;
     }
 
     public boolean deleteById(int id) {
-        return tx(session -> {
-            int deletedCount = session.createQuery("DELETE FROM User WHERE id = :id")
-                    .setParameter("id", id)
-                    .executeUpdate();
-            return deletedCount > 0;
-        });
+        crudRepository.run(
+                "DELETE FROM User WHERE id = :id",
+                Map.of("id", id)
+        );
+        return true;
     }
 
     public boolean updateUser(User user) {
-        return tx(session -> {
-            int updatedCount = session.createQuery(
-                            "UPDATE User SET name = :name, login = :login, password = :password WHERE id = :id"
-                    )
-                    .setParameter("name", user.getName())
-                    .setParameter("login", user.getLogin())
-                    .setParameter("password", user.getPassword())
-                    .setParameter("id", user.getId())
-                    .executeUpdate();
-            return updatedCount > 0;
-        });
-    }
-
-    private <T> T tx(final Function<Session, T> command) {
-        final Session session = sf.openSession();
-        final Transaction tx = session.beginTransaction();
-        try {
-            T rsl = command.apply(session);
-            tx.commit();
-            return rsl;
-        } catch (final Exception e) {
-            session.getTransaction().rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
+        crudRepository.run(
+                "UPDATE User SET name = :name, login = :login, password = :password WHERE id = :id",
+                Map.of(
+                        "name", user.getName(),
+                        "login", user.getLogin(),
+                        "password", user.getPassword(),
+                        "id", user.getId()
+                )
+        );
+        return true;
     }
 }
